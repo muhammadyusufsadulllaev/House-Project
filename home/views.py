0bb841cdc06e5a616cb828852289de5504d1a8e1
Muhammad Yusuf Sadullaev <mukhammadsadullaev@gmail.com>
2022-09-02 17:33:51 +0500

    New commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views import View
+import time
+
+
